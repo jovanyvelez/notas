f3bc8cd0dcc4f1a29630150fb66aaca192206256
Jovany <jovany.velez@gmail.com>
2024-07-11 10:41:40 -0500

    inicio

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { applyAction } from '$app/forms';
+
+  import "../app.css";
+  let {children} = $props();
+
+</script>
+
+
+
+{#if children}
+  {@render children()}
+{:else}
+   <h1>No existe una página principo</h1>
+{/if}
+
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+
+<header class="mt-10">
+    <h1 class="text-center text-3xl">Clase 1</h1>
+</header>
+
+<main class="">
+    <h1>Referencias</h1>
+</main>
