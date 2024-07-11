3ea527604307a7e730bbbfb2ee793a9de20d91e2
Jovany <jovany.velez@gmail.com>
2024-07-11 11:08:19 -0500

    Clase 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,22 @@
     <h1 class="text-center text-3xl">Clase 1</h1>
 </header>
 
-<main class="">
-    <h1>Referencias</h1>
+<main class="ml-10">
+    <h1 class="text-bold text-2xl mb-5">Referencias</h1>
+    <div class="flex flex-col">
+        <ul class="text-blue-500 ml-5">
+            <li>
+                <a href="https://docs.google.com/document/d/1S9Usx8vKUJ_QKQ2L5BpUPY1ELMsOVyORdDyhLbeCb60/edit?usp=sharing" target="_blank">Notas de Clase 1</a>
+            </li>
+            <li>
+                <a href="https://drive.google.com/file/d/1c4EBaiNDRmVfSo6iIaRmk7UYSBjDNfYG/view?usp=sharing" target="_blank">Base de datos ejemplo</a>
+            </li>
+            <li>
+                <a href="https://cs50.harvard.edu/sql/2024/notes/0/" target="_blank">Notas SQL</a>
+            </li>
+            <li>
+                <a href="https://sqliteonline.com" target="_blank">Herramienta de Practica</a>
+            </li>
+        </ul>
+    </div>
 </main>
