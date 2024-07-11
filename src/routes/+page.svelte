b7d9b1dde6de3b7e703330e07a490accf4f25269
Jovany <jovany.velez@gmail.com>
2024-07-11 11:12:10 -0500

    Clase 11

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,13 +11,13 @@
                 <a href="https://docs.google.com/document/d/1S9Usx8vKUJ_QKQ2L5BpUPY1ELMsOVyORdDyhLbeCb60/edit?usp=sharing" target="_blank">Notas de Clase 1</a>
             </li>
             <li>
-                <a href="https://drive.google.com/file/d/1c4EBaiNDRmVfSo6iIaRmk7UYSBjDNfYG/view?usp=sharing" target="_blank">Base de datos ejemplo</a>
-            </li>
-            <li>
                 <a href="https://cs50.harvard.edu/sql/2024/notes/0/" target="_blank">Notas SQL</a>
             </li>
             <li>
                 <a href="https://sqliteonline.com" target="_blank">Herramienta de Practica</a>
+            </li>
+            <li>
+                <a href="https://drive.google.com/file/d/1c4EBaiNDRmVfSo6iIaRmk7UYSBjDNfYG/view?usp=sharing" target="_blank">Base de datos ejemplo</a>
             </li>
         </ul>
     </div>
